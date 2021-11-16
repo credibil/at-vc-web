c98e27864b471c33fab04887635e28969b8b2b15
Your Name <you@example.com>
2021-11-16 14:19:21 +1300

    journey planner box

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -9,6 +9,10 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import ImportExportIcon from '@mui/icons-material/ImportExport';
 import { styled } from '@mui/material/styles';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+import TrainIcon from '@mui/icons-material/Train';
+import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
 
 const TextInput = styled(TextField)({
     marginTop: '0.5rem',
@@ -18,13 +22,21 @@ const TextInput = styled(TextField)({
     backgroundColor: '#fff',
 });
 
+const tabArray = [
+    { label: "Journey Planner", icon: <RoomIcon />, value: '1' },
+    { label: "Live Departures", icon: <DepartureBoardIcon />, value: '2' },
+    { label: "Timetables", icon: <ListAltOutlinedIcon />, value: '3' },
+]
+
+const transportArray = [
+    { label: "Bus", icon: <DirectionsBusIcon />, value: '1', description: 'Bus or route number', input: 'e.g. 161 or New Lynn' },
+    { label: "Train", icon: <TrainIcon />, value: '2', description: 'Train line or destination', input: 'e.g. South or Britomart' },
+    { label: "Ferry", icon: <DirectionsBoatIcon />, value: '3', description: 'Ferry route or destination', input: 'e.g. Half moon bay' },
+]
+
 export const Home = () => {
     const [value, setValue] = useState('1');
-    const tabArray = [
-        { label: "Journey Planner", icon: <RoomIcon />, value: '1' },
-        { label: "Live Departures", icon: <DepartureBoardIcon />, value: '2' },
-        { label: "Timetables", icon: <ListAltOutlinedIcon />, value: '3' },
-    ]
+    const [transportVal, setTransportVal] = useState('1');
 
     return (
         <Grid spacing={4} container>
@@ -63,8 +75,34 @@ export const Home = () => {
                     }
                     {value === '2' &&
                         <Box sx={{ color: 'background.default', p: 4 }}>
-                            <Box sx={{ typography: 'h4', pb: 2, color: "background.paper" }} > Search for a stop</Box>
+                            <Box sx={{ typography: 'h5', pb: 2, color: "background.paper" }} > Search for a stop</Box>
                             <TextInput fullWidth variant="filled" label="Location or stop number" />
+                        </Box>
+                    }
+                    {value === '3' &&
+                        <Box sx={{ color: 'background.default', p: 4 }}>
+                            <Box sx={{ typography: 'h5', pb: 2, color: "background.paper" }} > Transport mode</Box>
+                            <ButtonGroup fullWidth color="inherit">
+                                {transportArray.map((item, i) =>
+                                    <Button
+                                        sx={transportVal === item.value
+                                            ? { '& svg': { color: 'background.paper' } }
+                                            : {
+                                                backgroundColor: 'background.paper', '&:hover': { backgroundColor: 'background.paper' }, '& svg': { color: 'primary.dark' }
+                                            }}
+                                        variant={transportVal === item.value ? 'outlined' : 'text'}
+                                        onClick={() => (setTransportVal(item.value))}
+                                        key={i}>
+                                        {item.icon}
+                                    </Button>
+                                )}
+                            </ButtonGroup>
+                            {transportArray.map((item, i) => item.value === transportVal &&
+                                <div key={i}>
+                                    <Box sx={{ typography: 'h5', color: "background.paper", pt: 4 }} >{item.description}</Box>
+                                    <TextInput fullWidth variant="filled" label={item.input} />
+                                </div>
+                            )}
                         </Box>
                     }
                 </Box>
