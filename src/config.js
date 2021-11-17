1a275b231659bed2814b084e073b318fd0254785
Your Name <you@example.com>
2021-11-17 15:29:41 +1300

    qr code coming from backend

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const config = {
+    url: 'http://cc50-2407-7000-81b8-fa00-796c-a2e0-4dd5-7a4.ngrok.io',
+    getReqInit: {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+            Accept: 'application/ json',
+            'Content-Type': 'application/json'
+        },
+    }
+}
--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import CardMedia from '@mui/material/CardMedia';
 
 import { HomeIcon } from '../../Icons';
+import { config } from '../../config';
 
 const TextInput = styled(TextField)({
     marginTop: '0.5rem',
@@ -15,11 +18,35 @@ const TextInput = styled(TextField)({
     backgroundColor: '#fff',
 });
 
-const Div = styled(Box)({
-    marginTop: '2rem',
-})
 
 export const Login = () => {
+    const [qr, useQr] = useState([])
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setDialogOpen(true);
+    };
+
+    const handleDialogClose = () => {
+        setDialogOpen(false);
+    };
+    const appUrl = `${config.url}/issuer/issuance`;
+    const reqInit = config.getReqInit;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(appUrl, reqInit);
+                const json = await response.json();
+                useQr(json);
+            } catch (error) {
+                console.log("error", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <Box sx={{ backgroundColor: 'primary.dark', width: 500, p: 3, borderRadius: 1 }}>
             <Box sx={{ display: 'flex', typography: 'h2', alignItems: 'center', color: 'background.paper' }}>
@@ -29,7 +56,16 @@ export const Login = () => {
             <TextInput sx={{ mt: 5 }} fullWidth variant="filled" label="Email Address" />
             <TextInput fullWidth variant="filled" label="Password" />
             <Button sx={{ px: 3, mt: 2 }} color="primary" variant="contained">Log in</Button>
-            <Div sx={{ borderTop: 1, borderColor: 'grey.500' }} />
+            <Box sx={{ mt: 4, borderTop: 1, borderColor: 'grey.500' }} />
+            <Dialog fullWidth onClose={handleDialogClose} open={dialogOpen}>
+                <CardMedia
+                    component="img"
+                    height="100%"
+                    image={qr.qrCode}
+                    alt="qr code"
+                />
+            </Dialog>
+            <Button onClick={handleClickOpen}>Scan Here</Button>
         </Box>
     )
 }
