34768a68c5b6f5cc02ee69a4e63e0470e04f0ef1
finest08 <mr.mitcholl@gmail.com>
2021-11-22 17:31:25 +1300

    verifyVC

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
     url: 'https://f4fc-2407-7000-81b8-fa00-d5d5-c1eb-a557-3ac8.ngrok.io',
-    urlAlt: 'https://9aab-125-237-41-223.ngrok.io',
+    urlAlt: 'https://2180-125-237-41-223.ngrok.io',
 
     getReqInit: {
         method: "GET",
--- src/views/Verify/index.jsx
+++ src/views/Verify/index.jsx
@@ -1,65 +1,78 @@
 import React, { useEffect, useState } from 'react';
 
 import Box from '@mui/material/Box';
-import CardMedia from '@mui/material/CardMedia';
-import { config } from '../../config';
-
+import { config } from '@config';
 
 
 export const Verify = () => {
-    const [qr, useQr] = useState([])
-    const [status, setStatus] = useState();
-    const appUrl = `${config.urlAlt}/verifier/presentation`;
+    const [qrCode, setQRCode] = useState("");
+    const [status, setStatus] = useState({});
     const reqInit = config.getReqInit;
 
     useEffect(() => {
-        const fetchData = async () => {
+        let intervalId;
+
+        const verifyVC = async () => {
             try {
-                const response = await fetch(appUrl, reqInit);
-                const json = await response.json();
-                useQr(json);
+                const rsp = await fetch(`${config.urlAlt}/verifier/presentation`, reqInit);
+                const json = await rsp.json();
+                setQRCode(json.qrCode);
+                window.localStorage.setItem("state", JSON.stringify(json.state));
+
+                // set timer to check for state change (every 5 secs)
+                intervalId = setInterval(async () => {
+                    const state = window.localStorage.getItem("state")
+                    const rsp = await fetch(`${config.urlAlt}/verifier/status/${state}`, reqInit);
+                    const json = await rsp.json();
+                    setStatus(json);
+                }, 5000);
             } catch (error) {
                 console.log("error", error);
             }
         };
-        fetchData();
-    }, []);
+        verifyVC();
 
-    console.log("****", qr)
+        // cleanup timer when component is unloaded
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, [])
 
 
-    const appStatus = `${config.urlAlt}/verifier/status/${qr.state}`;
-    useEffect(() => {
-        const fetchState = async () => {
-            try {
-                const response = await fetch(appStatus, reqInit);
-                const json = await response.json();
-                setStatus(json);
-            } catch (error) {
-                console.log("error", error);
-            }
-        };
-        fetchState();
-    }, []);
-
-    console.log("STATUS", status)
     return (
         <Box>
-
             <Box>
-                <CardMedia
-                    component="img"
-                    height="100%"
-                    image={qr.qrCode}
-                    alt="qr code"
-                />
+                <Box display='flex' justifyContent='center' alignItems='center'>
+                    <img src={qrCode} alt="qrCode" />
+                </Box>
+                <Box sx={{ typography: 'h4', mt: 2 }}>
+                    Scan QR code to verify yourself with Microsoft Authenticator
+                </Box>
+                <Box sx={{ typography: 'h4', mt: 2 }}>
+                    {status.message}
+                </Box>
             </Box>
-            <Box sx={{ typography: 'h4', mt: 2 }}>
-                Scan QR code to verify yourself with Microsoft Authenticator
-            </Box>
-
         </Box>
     )
 }
 
-export default Verify
+export default Verify;
+
+function useLocalStorage(key) {
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            const item = window.localStorage.getItem([]);
+            return item ? JSON.parse(item) : [];
+        } catch (error) {
+            return [];
+        }
+    });
+    const useQr = (value) => {
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch (error) { }
+    };
+    return [storedValue, useQr];
+}
