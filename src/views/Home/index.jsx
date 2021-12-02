1eba70b01648afb8397b92004e522445e488e63c
Your Name <you@example.com>
2021-12-03 08:50:42 +1300

    view md

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -43,7 +43,7 @@ export const Home = () => {
     const [transportVal, setTransportVal] = useState('1');
 
     return (
-        <CardMedia sx={{ py: 10, m: '0 calc(50% - 50vw)', display: 'flex', justifyContent: 'center', lg: { height: 500 } }} image={Background} alt="">
+        <CardMedia sx={{ py: 10, m: '0 calc(50% - 50vw)', display: 'flex', justifyContent: 'center', md: { height: 500 } }} image={Background} alt="">
             <Grid container spacing={4} container sx={{ maxWidth: 'lg', px: 2 }}>
                 <Grid item xs={12} md={6} >
                     <Box sx={{ backgroundColor: 'secondary.main', display: 'flex' }}>
