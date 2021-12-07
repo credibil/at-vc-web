6b521ae86ab500fa017da08b1324dd41ba2f7ea0
Your Name <you@example.com>
2021-12-08 10:23:27 +1300

    [tag:patch] grid

--- src/views/Home/section.jsx
+++ src/views/Home/section.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import Container from '@mui/material/Container';
 import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid';
 import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
 import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
 
@@ -17,12 +19,22 @@ const tabs = [
 
 export const Section = () => {
     return (
-        <Box sx={{ display: 'flex', px: 6, py: 2, backgroundColor: 'grey.100', margin: '0 calc(50% - 50vw)', }}>
-            {tabs.map((item, i) =>
-                <Card sx={{ borderLeft: 2, borderColor: 'primary.main' }} key={i}>
-                    <CardHeader avatar={item.icon} title={item.label} />
-                </Card>
-            )}
+        <Box sx={{ px: 6, py: 2, backgroundColor: 'grey.100', margin: '0 calc(50% - 50vw)' }}>
+            <Container maxWidth="lg">
+                <Grid container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center" spacing={2} >
+                    {tabs.map((item, i) =>
+                        <Grid xs={6} md={3} key={i} item>
+                            <Card sx={{ borderLeft: 4, borderColor: 'primary.main' }} >
+                                <CardHeader avatar={item.icon} title={item.label} />
+                            </Card>
+                        </Grid>
+
+                    )}
+                </Grid>
+            </Container>
         </Box>
     )
 }
